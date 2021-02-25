01dbd1a01b44568a28babe3493274d45f607b92b
Турганбай С <turganbay.030282@gmail.com>
2021-02-26 00:45:54 +0600

    rename element-starter to frontent

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <div>
+      <hotels/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    Hotels: () => import("./components/Hotels"),
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, sans-serif;
+  text-align: center;
+}
+</style>
